76c160b51a7c5787be6381393c5f1b756ea48d8a
Reham Ahmed <rehamahmedfatima@gmail.com>
2026-08-02 16:05:03 +0300

    Update mood_providers.dart

--- flutter_app/lib/features/mood/presentation/providers/mood_providers.dart
+++ flutter_app/lib/features/mood/presentation/providers/mood_providers.dart
@@ -1,1 +1,35 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../data/supabase_mood_repository.dart';
+import '../../domain/mood_entry.dart';
+import '../../domain/mood_repository.dart';
 
+final moodRepositoryProvider = Provider<MoodRepository>((ref) => SupabaseMoodRepository());
+
+class MoodHistoryNotifier extends StateNotifier<AsyncValue<List<MoodLog>>> {
+  MoodHistoryNotifier(this._repo) : super(const AsyncValue.loading()) {
+    refresh();
+  }
+  final MoodRepository _repo;
+
+  Future<void> refresh() async {
+    state = const AsyncValue.loading();
+    try {
+      state = AsyncValue.data(await _repo.loadHistory());
+    } catch (e, st) {
+      state = AsyncValue.error(e, st);
+    }
+  }
+
+  Future<void> log(String mood, int score) async {
+    await _repo.logMood(mood: mood, score: score);
+    await refresh();
+  }
+}
+
+final moodHistoryProvider = StateNotifierProvider<MoodHistoryNotifier, AsyncValue<List<MoodLog>>>(
+  (ref) => MoodHistoryNotifier(ref.watch(moodRepositoryProvider)),
+);
+
+final suggestedActivitiesProvider = FutureProvider<List<String>>((ref) {
+  return ref.watch(moodRepositoryProvider).suggestActivities();
+});
