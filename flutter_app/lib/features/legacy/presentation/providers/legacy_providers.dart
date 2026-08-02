ac6b315e01fb964574a37358623e8d0c1ed2ec1e
Reham Ahmed <rehamahmedfatima@gmail.com>
2026-08-02 14:00:32 +0300

    Add legacy providers for repository and profile

--- flutter_app/lib/features/legacy/presentation/providers/legacy_providers.dart
+++ flutter_app/lib/features/legacy/presentation/providers/legacy_providers.dart
@@ -1,1 +1,14 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../data/supabase_legacy_repository.dart';
+import '../../domain/legacy_profile.dart';
+import '../../domain/legacy_repository.dart';
 
+final legacyRepositoryProvider = Provider<LegacyRepository>((ref) => SupabaseLegacyRepository());
+
+final legacyProfileProvider = FutureProvider.autoDispose<LegacyProfile>((ref) {
+  return ref.watch(legacyRepositoryProvider).load();
+});
+
+final legacyAlignmentProvider = FutureProvider.autoDispose<String>((ref) {
+  return ref.watch(legacyRepositoryProvider).checkAlignment();
+});
