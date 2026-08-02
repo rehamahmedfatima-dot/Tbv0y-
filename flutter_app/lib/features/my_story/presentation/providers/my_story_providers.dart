81c67dba94abb13ccedfa9eac5eb260d758aeee7
Reham Ahmed <rehamahmedfatima@gmail.com>
2026-08-02 16:21:04 +0300

    Update my_story_providers.dart

--- flutter_app/lib/features/my_story/presentation/providers/my_story_providers.dart
+++ flutter_app/lib/features/my_story/presentation/providers/my_story_providers.dart
@@ -1,1 +1,29 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../data/supabase_my_story_repository.dart';
+import '../../domain/my_story_repository.dart';
+import '../../domain/story_report.dart';
 
+final myStoryRepositoryProvider = Provider<MyStoryRepository>((ref) => SupabaseMyStoryRepository());
+
+final currentYearReportProvider = FutureProvider.autoDispose<StoryReport?>((ref) {
+  return ref.watch(myStoryRepositoryProvider).loadReport(DateTime.now().year);
+});
+
+class StoryGenerationController extends StateNotifier<AsyncValue<StoryReport?>> {
+  StoryGenerationController(this._repo) : super(const AsyncValue.data(null));
+  final MyStoryRepository _repo;
+
+  Future<void> generate(int year) async {
+    state = const AsyncValue.loading();
+    try {
+      state = AsyncValue.data(await _repo.generateReport(year));
+    } catch (e, st) {
+      state = AsyncValue.error(e, st);
+    }
+  }
+}
+
+final storyGenerationProvider =
+    StateNotifierProvider<StoryGenerationController, AsyncValue<StoryReport?>>(
+  (ref) => StoryGenerationController(ref.watch(myStoryRepositoryProvider)),
+);
