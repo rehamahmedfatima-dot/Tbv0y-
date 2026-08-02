1823b464f0295ba5373d905c623bf77ae9d34b88
Reham Ahmed <rehamahmedfatima@gmail.com>
2026-08-02 16:31:20 +0300

    Add providers for time machine feature

--- flutter_app/lib/features/time_machine/presentation/providers/time_machine_providers.dart
+++ flutter_app/lib/features/time_machine/presentation/providers/time_machine_providers.dart
@@ -1,1 +1,14 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../data/supabase_time_machine_repository.dart';
+import '../../domain/day_replay.dart';
+import '../../domain/time_machine_repository.dart';
 
+final timeMachineRepositoryProvider =
+    Provider<TimeMachineRepository>((ref) => SupabaseTimeMachineRepository());
+
+final selectedReplayDateProvider = StateProvider<DateTime>((ref) => DateTime.now());
+
+final dayReplayProvider = FutureProvider.autoDispose<DayReplay>((ref) {
+  final date = ref.watch(selectedReplayDateProvider);
+  return ref.watch(timeMachineRepositoryProvider).loadDay(date);
+});
